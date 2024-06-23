468d28500868866c4d05707a418f133089771ff2
haze_e <135668188+HazeeOnYT@users.noreply.github.com>
2024-06-22 19:25:17 -0600

    Update fynsoskid-NOWARNINGS.bat

--- fynsoskid-NOWARNINGS.bat
+++ fynsoskid-NOWARNINGS.bat
@@ -1,19 +1,20 @@
 @echo off
 setlocal
 
-:: Download stuff
-curl -Lo mlt.cpp https://github.com/pankoza2-pl/gdithings/raw/2473c777c6cdd43f8c6965b63bd715e04a0786bd/mlt.cpp || exit /b 1
+:: Download mlt.cpp from GitHub
+curl -Lo mlt.cpp https://github.com/pankoza2-pl/gdithings/raw/2473c777c6cdd43f8c6965b63bd715e04a0786bd/mlt.cpp
 
-:: Install MinGW if not installed
+:: Check if MinGW is installed
 where g++ || (
-    curl -Lo mingw.7z "https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/8.1.0/threads-posix/dwarf/i686-8.1.0-release-posix-dwarf-rt_v6-rev0.7z/download" || exit /b 1
+    :: Download and extract MinGW if not installed
+    curl -Lo mingw.7z "https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/8.1.0/threads-posix/dwarf/i686-8.1.0-release-posix-dwarf-rt_v6-rev0.7z/download"
     7z x mingw.7z -omgw && set PATH=%PATH%;%CD%\mingw\bin
 )
 
-:: Converting stuff
-g++ mlt.cpp -o mlt.exe || exit /b 1
+:: Compile mlt.cpp to mlt.exe using MinGW
+g++ mlt.cpp -o mlt.exe
 
-:: Running stuff
-start mlt.exe
+:: Run mlt.exe
+mlt.exe
 
 endlocal
